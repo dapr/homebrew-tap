349973854e9929f43f13774c9f63044b446a618e
Artur Souza <artursouza.ms@outlook.com>
2020-04-02 10:45:34 -0700

    Updating verison to v0.6.0

--- dapr-cli.rb
+++ dapr-cli.rb
@@ -6,17 +6,11 @@
 class DaprCli < Formula
   desc "Client for Dapr."
   homepage "https://dapr.io"
-  version '0.6.0-rc.0'
-  url "https://github.com/dapr/cli/archive/v0.6.0-rc.0.tar.gz"
-  sha256 "0325902d3f5f08dabe61dfe8fe27469b901d8627947d2ef46902fbcfcb18a267"
+  version '0.6.0'
+  url "https://github.com/dapr/cli/archive/v0.6.0.tar.gz"
+  sha256 "8df5d6e852397481377b10a7c5062c187c191678a0c254a98ccc5c71200c73a7"
 
   depends_on "go" => :build
-
-  bottle do
-    root_url "https://dl.bintray.com/dapr/bottles-dapr"
-    cellar :any_skip_relocation
-    sha256 "75fa06dedb6a00896ddf4458e46685ab16d292dc492ea834ef106349ed15e1e2" => :catalina
-  end
 
   def install
     system "go", "build", "-ldflags", "-X main.version=#{version} -X main.apiVersion=1.0", "-o", "./cli"

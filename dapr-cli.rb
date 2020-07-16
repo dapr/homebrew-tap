f24e4ffec1363ea825b393495772a1e53dc659f6
daprweb@microsoft.com <Dapr Bot>
2020-07-16 18:53:07 +0000

    Updating dapr-cli formula for version 0.9.0 skip-checks: true

--- dapr-cli.rb
+++ dapr-cli.rb
@@ -6,16 +6,16 @@
 class DaprCli < Formula
   desc "Client for Dapr."
   homepage "https://dapr.io"
-  version '0.8.0'
-  url "https://github.com/dapr/cli/archive/v0.8.0.tar.gz"
-  sha256 "2688988e6d17192b34c7426544bfd242695ad7178a241130767b9b79388e052e"
+  version '0.9.0'
+  url "https://github.com/dapr/cli/archive/v0.9.0.tar.gz"
+  sha256 "91955867387f2004374027d9302a75746d67334f59cbc5f17c87ee52d71f27fe"
 
   depends_on "go" => :build
 
   bottle do
-    root_url "https://github.com/dapr/homebrew-tap/releases/download/v0.8.0"
+    root_url "https://github.com/dapr/homebrew-tap/releases/download/v0.9.0"
     cellar :any_skip_relocation
-    sha256 "c9766867b63b8aeffe9021eb78ab0be22cf105dc075169131eb73017aa43674d" => :catalina
+    sha256 "1040f244c191f71b843226708a532d980deacc2777a32d6951139046a11f78fa" => :catalina
   end
 
   def install

df536e3529b3ff7a7afa5a3e43cd518d97686128
daprweb@microsoft.com <Dapr Bot>
2021-02-11 03:12:33 +0000

    Updating dapr-cli@1.0.0-rc.5 formula for version 1.0.0-rc.5 skip-checks: true

---
+++ dapr-cli@1.0.0-rc.5.rb
@@ -0,0 +1,30 @@
+# ------------------------------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------------------------------
+
+class DaprCliAT100Rc5 < Formula
+  desc "Client for Dapr."
+  homepage "https://dapr.io"
+  version '1.0.0-rc.5'
+  url "https://github.com/dapr/cli/archive/v1.0.0-rc.5.tar.gz"
+  sha256 "8b0bb2a0a9c49b720563c28b477803ff06547d6d3dd105d5c5801ad1377207e8"
+
+  depends_on "go" => :build
+
+  bottle do
+    root_url "https://github.com/dapr/homebrew-tap/releases/download/v1.0.0-rc.5"
+    cellar :any_skip_relocation
+    sha256 "8d7b70d4335ed6718ec6156219f7fba1e366a6abc99ecf62860b9e5a37c60a03" => :catalina
+  end
+
+  def install
+    system "go", "build", "-ldflags", "-X main.version=#{version} -X main.apiVersion=1.0", "-o", "./cli"
+    bin.install "cli" => "dapr"
+  end
+
+  test do
+    system "#{bin}/dapr", "--version"
+  end
+end
+

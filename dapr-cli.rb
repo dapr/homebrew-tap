73a693f41f184490e64ce867c2c2a47638ac9a54
alswl <alswlx@gmail.com>
2021-04-12 13:32:08 +0800

    fix: style fix for homebrew

--- dapr-cli.rb
+++ dapr-cli.rb
@@ -4,19 +4,18 @@
 # ------------------------------------------------------------
 
 class DaprCli < Formula
-  desc "Client for Dapr."
+  desc "Client for Dapr"
   homepage "https://dapr.io"
-  version '1.1.0'
   url "https://github.com/dapr/cli/archive/v1.1.0.tar.gz"
+  version "1.1.0"
   sha256 "e34389a4491b071bdd915d72adaf2ee8273d0da90b4178a16c88c28ceaba2717"
-
-  depends_on "go" => :build
 
   bottle do
     root_url "https://github.com/dapr/homebrew-tap/releases/download/v1.1.0"
-    cellar :any_skip_relocation
-    sha256 "818bd14f4c8b05ff249444e0e1e7b98ba5183f2342d82481709df687b8573d5d" => :catalina
+    sha256 cellar: :any_skip_relocation, catalina: "818bd14f4c8b05ff249444e0e1e7b98ba5183f2342d82481709df687b8573d5d"
   end
+
+  depends_on "go" => :build
 
   def install
     system "go", "build", "-ldflags", "-X main.version=#{version} -X main.apiVersion=1.0", "-o", "./cli"
@@ -27,4 +26,3 @@ class DaprCli < Formula
     system "#{bin}/dapr", "--version"
   end
 end
-

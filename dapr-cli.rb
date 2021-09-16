3a3a941ade5de89d35a6e2bb00bf09207c4a8de3
daprweb@microsoft.com <Dapr Bot>
2021-09-16 04:13:44 +0000

    Updating dapr-cli formula for version 1.4.0 skip-checks: true

--- dapr-cli.rb
+++ dapr-cli.rb
@@ -6,20 +6,21 @@
 class DaprCli < Formula
   desc "Client for Dapr."
   homepage "https://dapr.io"
-  version '1.3.0'
-  url "https://github.com/dapr/cli/archive/v1.3.0.tar.gz"
-  sha256 "e12e8824714ad4adb67790689f8d299f8ec68f53acf69ff0f88d986c0fb509cc"
+  version '1.4.0'
+  url "https://github.com/dapr/cli/archive/v1.4.0.tar.gz"
+  sha256 "a6ef9df946d6aebc280c6eeb7b2c4dd78738f726e1da037a4cf160fe16541065"
 
   depends_on "go" => :build
+  depends_on "make" => :build
 
   bottle do
-    root_url "https://github.com/dapr/homebrew-tap/releases/download/v1.3.0"
-    sha256 cellar: :any_skip_relocation, catalina: "e0180d89af41dd591ff9ff2ed8d2ef8f5e664bd665ea7a690961bf279e6065d9"
+    root_url "https://github.com/dapr/homebrew-tap/releases/download/v1.4.0"
+    sha256 cellar: :any_skip_relocation, catalina: "4f7e4105ba56b079c1b3a83960da9cd4d26979034dbf2c9de5466a5853f2cb9b"
   end
 
   def install
-    system "go", "build", "-ldflags", "-X main.version=#{version} -X main.apiVersion=1.0", "-o", "./cli"
-    bin.install "cli" => "dapr"
+    system "make", "REL_VERSION=#{version}"
+    bin.install "dist/darwin_amd64/release/dapr" => "dapr"
   end
 
   test do

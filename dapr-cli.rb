74ae51f32720328d9c8c6f82403650f04bbf51d6
daprweb@microsoft.com <Dapr Bot>
2021-11-11 22:34:19 +0000

    Updating dapr-cli formula for version 1.5.0 skip-checks: true

--- dapr-cli.rb
+++ dapr-cli.rb
@@ -6,16 +6,16 @@
 class DaprCli < Formula
   desc "Client for Dapr."
   homepage "https://dapr.io"
-  version '1.4.0'
-  url "https://github.com/dapr/cli/archive/v1.4.0.tar.gz"
-  sha256 "a6ef9df946d6aebc280c6eeb7b2c4dd78738f726e1da037a4cf160fe16541065"
+  version '1.5.0'
+  url "https://github.com/dapr/cli/archive/v1.5.0.tar.gz"
+  sha256 "3c7d82fa071337e1ce0adc2257d47a0fd39895c6f8ae42519688ead0e73b7683"
 
   depends_on "go" => :build
   depends_on "make" => :build
 
   bottle do
-    root_url "https://github.com/dapr/homebrew-tap/releases/download/v1.4.0"
-    sha256 cellar: :any_skip_relocation, catalina: "4f7e4105ba56b079c1b3a83960da9cd4d26979034dbf2c9de5466a5853f2cb9b"
+    root_url "https://github.com/dapr/homebrew-tap/releases/download/v1.5.0"
+    sha256 cellar: :any_skip_relocation, big_sur: "266d184344283dc7ffdf2dc62db469ce94c0c3cd2c94c5ddc17dc3ee081f7b4d"
   end
 
   def install

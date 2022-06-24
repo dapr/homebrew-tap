cf7cdb3918d65f8f609eebba27419d985a523105
Pravin Pushkar <ppushkar@microsoft.com>
2022-06-24 11:05:17 +0530

    updating year to 2022 in copyright
    Signed-off-by: Pravin Pushkar <ppushkar@microsoft.com>

--- .github/scripts/generate-formula.sh
+++ .github/scripts/generate-formula.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ------------------------------------------------------------
-# Copyright 2021 The Dapr Authors
+# Copyright 2022 The Dapr Authors
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -16,7 +16,7 @@
 set -ue
 
 echo "# ------------------------------------------------------------
-# Copyright 2021 The Dapr Authors
+# Copyright 2022 The Dapr Authors
 # Licensed under the Apache License, Version 2.0 (the \"License\");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
